5166c391748ddbbd2b843656fea3b8c23f1c8c93
Miran San <miran.san@ozu.edu.tr>
2022-12-24 15:17:32 +0300

    odev

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,67 @@
-import React from 'react'
-
-const URL = 'http://localhost:9000/api/todos'
+import React, { useEffect, useState } from "react";
+import Form from "./Form";
+import TodoList from "./TodoList";
+import axios from "axios";
 
 export default function App() {
-    return null
+  const [hideDone, setHideDone] = useState(false);
+
+  const [todos, setTodos] = useState([]);
+  
+
+
+
+  // etütte elemanda Todo.js içerisinde yaptığımızı burada yapıyoruz
+  const handleCheckItem = (id) => {
+    // todos arrayinin bir kopyasını oluştur
+    const todosC = [...todos];
+    // hangi ögeye tıklandı
+    const ilgiliOge = todosC.filter(i => i.id === id)[0];
+    // tıklanan ögenin tamamlandı değerini değiştir
+    ilgiliOge.tamamlandi = !ilgiliOge.tamamlandi;
+    // bu değişikliğe sahip arrayi todos statine yaz
+    setTodos(todosC);
+  }
+
+  useEffect(() => {
+    axios.get("http://localhost:9000/api/todos").then((res)=> {
+      setTodos(res.data.data)
+    } )})
+           
+  
+  
+
+
+
+  // etütte elemanda Form.js içerisinde yaptığımızı burada yapıyoruz
+  const handleSubmit = (yeni) => {   
+        
+    setTodos([
+      ...todos,
+      {
+        isim: yeni,
+        id: Date.now(),
+        tamamlandi: false,
+      },
+    ]);
+  }
+
+  const handleToggle = () => {
+    setHideDone(!hideDone)
+  }
+
+  return (
+    <div>
+      Todo App
+      <h1>Todos</h1>
+      <TodoList
+        list={hideDone ? todos.filter(oge => oge.tamamlandi !== true) : todos}
+        checkItem={handleCheckItem}
+
+      />
+      <Form doSubmit={handleSubmit} />
+
+      <button onClick={handleToggle}>tamamlananları {hideDone ? "Göster" : "Gizle" }</button>
+    </div>
+  );
 }
